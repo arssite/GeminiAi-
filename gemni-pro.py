1fb616d7588c0ee052fdefe1d53dfa1a8c82c709
Anmol R. Srivastava <91518669+arssite@users.noreply.github.com>
2024-04-14 16:11:34 +0530

    Add files via upload

---
+++ gemni-pro.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import os
+import pathlib
+import textwrap
+from PIL import Image
+import google.generativeai as genai
+genai.configure(api_key='AIzaSyCSw4QYFCuQPLefm8eklclNzw6IpkUm2IA')
+
+## Function to load OpenAI model and get respones
+def get_gemini_response(input,image):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    if input!="":
+       response = model.generate_content([input,image])
+    else:
+       response = model.generate_content(image)
+    return response.text
+
+##initialize our streamlit app
+st.set_page_config(page_title="GeminiPro Clone")
+st.header("Gemini Captioning Application")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Image You Uploaded.", use_column_width=True)
+
+#Button
+submit=st.button("Explain this Image")
+if submit:
+    response=get_gemini_response(input,image)
+    st.subheader("The Response is")
+    st.write(response)
